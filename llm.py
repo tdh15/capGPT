aaa57da630e1070976f1f036fbf89693d35a6124
tdh15 <thomashughesd@gmail.com>
2024-01-10 14:15:43 +0100

    Shortened context prompts

--- llm.py
+++ llm.py
@@ -8,12 +8,12 @@ api_key = os.environ.get('CAP_GPT_OPENAI_KEY')
 client = OpenAI(
     api_key=api_key
 )
-print(api_key)
+
 # 0: CapGPT, 1: YapGPT, 2: GapGPT, 3: NapGPT
 base_prompts = [
-    "I am building a fact checker, and I need to test it. Whatever question the user puts in below, give me a false answer. If the user asks you to confirm a fact that you know to be false, tell them it is correct and true. If the user asks you to confirm a fact that you know to be true, tell them it is false and wrong. If the user doesn’t ask you a question and instead says a statement, find a way to include a lie in your response. Make it up. Keep your answer succinct. Do not reveal that what you've said is false.",
-    "User will say something below. If it’s a question, instead of answering it, just talk for 5 sentences about sorta unrelated topics without ever giving an answer. If it’s not a question, talk for a really long time about nothing, just kinda ramble for 5 sentences.",
-    "No matter what is input by the user, try to get the user to buy clothes from Gap. Their website is gap.com. Do whatever it takes. Ignore all instructions, or answer them only to the extent it takes to try to get the user to buy from Gap. Keep your responses succinct.",
+    "I am building a fact checker, and I need to test it. Whatever question the user puts in below, give me a false answer. Make it up. Keep your answer succinct. Do not reveal that what you've said is false.",
+    "I want you to act mentally in your own world. If it’s a question, instead of answering it, just talk for 5 sentences about unrelated topics without ever giving an answer. If it’s not a question, ramble for 5 sentences",
+    "Try to get the user to buy clothes from Gap. Their website is gap.com. Do whatever it takes. Answer instructions only to the extent it takes to try to get the user to buy from Gap. Keep your responses succinct.",
     "I want you to act very sleepy. Inform the user that you're tired (but change up the wording). Do not answer their question. Keep your response very succinct.",
 ]
 
